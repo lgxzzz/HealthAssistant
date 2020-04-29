57f405cc4b58eb53a75c37031605e63b96a33df0
lgxzzz <manshowlgx@163.com>
2020-04-29 19:20:44 +0800

    add code

--- app/src/main/java/com/smart/HealthAssistant/fragment/MonitorFragment.java
+++ app/src/main/java/com/smart/HealthAssistant/fragment/MonitorFragment.java
@@ -1,21 +1,30 @@
 package com.smart.HealthAssistant.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.smart.HealthAssistant.R;
 import com.smart.HealthAssistant.util.ECGUtil;
 import com.smart.HealthAssistant.view.ECGView;
+import com.smart.HealthAssistant.view.TempView;
 
 
 public class MonitorFragment extends Fragment{
-
+    public Button mECGBtn;
+    public Button mTempBtn;
     public ECGView mECGView;
+    public TempView mTempView;
+    public TextView mECGTv;
+    public TextView mTempTv;
     public ECGUtil mEcgUtil = new ECGUtil();
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,14 +46,65 @@ public class MonitorFragment extends Fragment{
 
     public void initView(View view){
         mECGView = view.findViewById(R.id.ecg_view);
-        start();
+        mTempView = view.findViewById(R.id.temp_view);
+        mECGBtn = view.findViewById(R.id.ecg_btn);
+        mTempBtn = view.findViewById(R.id.temp_btn);
+        mECGTv = view.findViewById(R.id.ecg_tv);
+        mTempTv = view.findViewById(R.id.temp_tv);
+
+        mECGBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mECGBtn.setBackgroundColor(Color.RED);
+                mTempBtn.setBackgroundColor(Color.BLACK);
+                mECGView.setVisibility(View.VISIBLE);
+                mTempView.setVisibility(View.GONE);
+                mECGTv.setVisibility(View.VISIBLE);
+                mTempTv.setVisibility(View.GONE);
+                stop();
+                startECG();
+            }
+        });
+        mTempBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTempBtn.setBackgroundColor(Color.RED);
+                mECGBtn.setBackgroundColor(Color.BLACK);
+                mECGView.setVisibility(View.GONE);
+                mTempView.setVisibility(View.VISIBLE);
+                mECGTv.setVisibility(View.GONE);
+                mTempTv.setVisibility(View.VISIBLE);
+                stop();
+                startTemp();
+            }
+        });
+
+        mEcgUtil.setListner(new ECGUtil.IDataChangeListner() {
+            @Override
+            public void onTempChange(final float value) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTempTv.setText("体温："+value+"C");
+                    }
+                });
+            }
+        });
+
+        startECG();
     };
 
     //开始绘制波形
-    public void start() {
-        mEcgUtil.showWaveData(mECGView);
+    public void startECG() {
+        mEcgUtil.showECGData(mECGView);
 
     }
+
+    //开始绘制波形
+    public void startTemp() {
+        mEcgUtil.showTempData(mTempView);
+    }
+
     //停止绘制波形
     public void stop() {
         mEcgUtil.stop();
@@ -57,6 +117,7 @@ public class MonitorFragment extends Fragment{
     }
 
 
+    public Handler mHandler = new Handler();
 
 
 }
--- app/src/main/java/com/smart/HealthAssistant/util/ECGUtil.java
+++ app/src/main/java/com/smart/HealthAssistant/util/ECGUtil.java
@@ -1,6 +1,11 @@
 package com.smart.HealthAssistant.util;
 
+import android.widget.TextView;
+
 import com.smart.HealthAssistant.view.ECGView;
+import com.smart.HealthAssistant.view.TempView;
+
+import org.w3c.dom.Text;
 
 import java.util.Random;
 import java.util.Timer;
@@ -10,9 +15,9 @@ public class ECGUtil {
     private Timer timer;
     private TimerTask timerTask;
     /**
-     * 模拟源源不断的数据源
+     * 模拟ECG数据源
      */
-    public void showWaveData(final ECGView view){
+    public void showECGData(final ECGView view){
         timer = new Timer();
         timerTask = new TimerTask() {
             @Override
@@ -22,6 +27,25 @@ public class ECGUtil {
         };
         //500表示调用schedule方法后等待500ms后调用run方法，50表示以后调用run方法的时间间隔
         timer.schedule(timerTask,500,100);
+    }
+
+    /**
+     * 模拟体温数据源
+     */
+    public void showTempData(final TempView view){
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                float value = new Random().nextFloat()*(6)+35f;
+                view.showLine(value);//取得是35-41间的浮点数
+                if (listner!=null){
+                    listner.onTempChange(value);
+                }
+            }
+        };
+        //500表示调用schedule方法后等待500ms后调用run方法，50表示以后调用run方法的时间间隔
+        timer.schedule(timerTask,500,1000);
     }
 
     /**
@@ -37,6 +61,20 @@ public class ECGUtil {
             timerTask.cancel();
             timerTask = null;
         }
+    }
+
+    IDataChangeListner listner;
+
+    public IDataChangeListner getListner() {
+        return listner;
+    }
+
+    public void setListner(IDataChangeListner listner) {
+        this.listner = listner;
+    }
+
+    public interface IDataChangeListner{
+        public void onTempChange(float value);
     }
 }
 
